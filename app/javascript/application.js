75e37c08fbc3af8fd5662e32f8170a8a24f71e05
ZihanZhang <zihanzhang422@gmail.com>
2022-09-04 13:11:43 -0700

    final

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,16 +7,31 @@ import "./channels"
 import * as jquery from "jquery"
 import "semantic-ui" 
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    }
+  })
+}
+
 
 $(document).on('turbo:load', function() {
   $('.ui.dropdown').dropdown();
-})
-
-$('.message .close')
+  $('.message .close')
   .on('click', function() {
     $(this)
       .closest('.message')
       .transition('fade')
     ;
-  })
-;
+  });
+  submit_message();
+  scroll_bottom();
+})
